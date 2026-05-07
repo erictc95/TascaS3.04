545543ecbc31aea9db3ccbac6d8e80547143d219
erictc95 <maxerictc95@hotmail.com>
2026-05-07 13:37:55 +0200

    Configured Docker and MySQL JDBC connection

---
+++ src/main/java/com/agenda/infrastructure/sql/ConnectionManager.java
@@ -0,0 +1,17 @@
+package com.agenda.infrastructure.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static final String URL = "jdbc:mysql://localhost:3307/agenda_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+}

17b1ef37932dec67bb2c86c411a89a24db9e64d2
erictc95 <maxerictc95@hotmail.com>
2026-05-14 12:51:55 +0200

    feat: save() sql.

--- src/main/java/com/agenda/application/menu/Main.java
+++ src/main/java/com/agenda/application/menu/Main.java
@@ -3,12 +3,13 @@ package com.agenda.application.menu;
 import com.agenda.note.cli.NoteMenu;
 import com.agenda.note.repository.INoteRepository;
 import com.agenda.note.repository.NoteInMemoryRepository;
+import com.agenda.note.repository.NoteSqlRepository;
 import com.agenda.note.service.NoteService;
 
 public class Main {
 
     public static void main(String[] args) {
-        INoteRepository repository = new NoteInMemoryRepository();
+        INoteRepository repository = new NoteSqlRepository();
 
         NoteService service = new NoteService(repository);
 
--- src/main/java/com/agenda/note/repository/NoteSqlRepository.java
+++ src/main/java/com/agenda/note/repository/NoteSqlRepository.java
@@ -1,4 +1,70 @@
 package com.agenda.note.repository;
 
-public class NoteSqlRepository {
+import com.agenda.infrastructure.sql.ConnectionManager;
+import com.agenda.note.model.Note;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class NoteSqlRepository implements INoteRepository {
+
+
+    @Override
+    public void save(Note note) {
+        String sql = """
+                INSERT INTO note
+                (note_title, note_description, note_task_id)
+                VALUES (?, ?, ?)
+                """;
+
+        try (
+                Connection connection =
+                        ConnectionManager.getConnection();
+
+                PreparedStatement statement =
+                        connection.prepareStatement(sql)
+                ) {
+            statement.setString(1, note.getTitle());
+            statement.setString(2, note.getDescription());
+            statement.setInt(3, note.getTaskId());
+
+            statement.executeUpdate();
+
+            System.out.println("Note saved in database!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Note> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void update(Note note) {
+
+    }
+
+    @Override
+    public void delete(int noteId) {
+
+    }
+
+    @Override
+    public Note findById(int noteId) {
+        return null;
+    }
+
+    @Override
+    public List<Note> findByTitle(String title) {
+        return List.of();
+    }
+
+    @Override
+    public List<Note> findByWord(String word) {
+        return List.of();
+    }
 }
